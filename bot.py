7baae8535d9c2f21575d55930141ba588ac31606
Georgyyx <julyfortune101@gmail.com>
2022-07-13 17:26:12 +0300

    coding finished

--- bot.py
+++ bot.py
@@ -19,16 +19,23 @@ dp = Dispatcher(bot)
 
 p2p = AioQiwiP2P(auth_key=QIWI_KEY)
 
+
 @dp.message_handler(commands=['start'])
 async def start_conversation(message: types.Message):
+    """Client conversation entry point"""
     message_to_user = '''Привет, {}!\n\nЯ - бот для пополнения баланса. 
-Нажмите на кнопку, чтобы пополнить баланс
-Снизу инлайн кнопка с текстом "Пополнить баланс"'''
-
+        Нажмите на кнопку, чтобы пополнить баланс'''
     pay_inline_mu = types.InlineKeyboardMarkup().add(
         types.InlineKeyboardButton("Пополнить баланс", callback_data='pay_cb_data'))
+    application_to_user = message.from_user.username if message.from_user.username else 'пользователь'
     await message.reply(message_to_user.format(
-        message.from_user.username), reply_markup=pay_inline_mu)  # possible empty username
+        application_to_user), reply_markup=pay_inline_mu)
+
+
+@dp.message_handler(commands=['admin'])
+async def admin_handler(message: types.Message):
+    """Admin entry point"""
+    ...
 
 
 class Payment(StatesGroup):
@@ -44,19 +51,31 @@ async def inline_btn_cb_handler(query: types.CallbackQuery):
 
 
 async def write_payment(user_id, amount):
+    """Function handles accepted payment - writes changes to database"""
     conn = await asyncpg.connect(DSN)
-    on_start_query = """CREATE TABLE IF NOT EXISTS 
-         bot.clients(user_id BIGINT UNIQUE, amount INTEGER)"""
     get_current_amount_query = """SELECT amount FROM clients WHERE user_id = $1"""
     write_new_amount = """UPDATE clients SET amount = $1 WHERE user_id = $2"""
-    users_balance = await conn.fetchval(get_current_amount_query, user_id)  # no user?
+    users_balance = await conn.fetchval(get_current_amount_query, user_id)
+    if not users_balance:  # None if value not found
+        users_balance = 0
     balance_with_accepted = users_balance + amount
     await conn.execute(write_new_amount, balance_with_accepted, user_id)
     await conn.close()
 
 
+async def db_on_startup():
+    """Checks payments table in database, introduced in
+    DSN variable of .env file"""
+    conn = await asyncpg.connect(DSN)
+    on_start_query = """CREATE TABLE IF NOT EXISTS 
+         clients(user_id BIGINT UNIQUE, amount INTEGER)"""
+    await conn.execute(on_start_query)
+    await conn.close()
+
+
 @dp.message_handler(lambda message: message.text.isdigit(), state=Payment.payment_amount)
 async def process_sum(message: types.Message, state: FSMContext):
+    """Gets the amount of money for payment and creates bill with it as parameter"""
     new_bill = await p2p.bill(bill_id=str(uuid.uuid5(uuid.NAMESPACE_X500, str(message.from_id))),
                               amount=message.text, lifetime=5)
     async with state.proxy() as payment_data:
@@ -68,26 +87,30 @@ async def process_sum(message: types.Message, state: FSMContext):
     ).add(
         types.InlineKeyboardButton("Оплачено!", callback_data='check_qiwi')
     )
-    message_id = await message.reply('Платёж успешно создан!', reply_markup=payment_created_kb)
+    await message.reply('Платёж успешно создан!', reply_markup=payment_created_kb)
 
 
 @dp.callback_query_handler(text='check_qiwi')
 async def check_qiwi(cb: types.CallbackQuery, state: FSMContext):
+    """On `PAID` button click checks the pyament status through
+    qiwi p2p api and handles the result"""
     await cb.answer('Проверка оплаты')
     user_id = cb.from_user.id
     async with state.proxy as payment_data:
         bill_id = payment_data['bill_id']
     payment_status = (await p2p.check(bill_id=bill_id)).status
     match payment_status:
-        case['WAITING']:
+        case ['WAITING']:
             await bot.send_message(user_id, "Сервис ожидает оплату")
-        case['PAID']:
+        case ['PAID']:
             async with state.proxy() as payment_data:
                 bill_amount = payment_data['amount']
             await write_payment(user_id, bill_amount)
-        case['REJECTED']:
-            ...
-        case['EXPIRED']:
+        case ['REJECTED']:
+            await bot.send_message(user_id, "Платёж отменён")
+        case ['EXPIRED']:
+            await bot.send_message(user_id,
+                                   'Срок действия платежа истёк')
 
 
 @dp.message_handler(lambda message: not message.text.isdigit(), state=Payment.payment_amount)
@@ -97,4 +120,5 @@ async def process_sum_invalid(message: types.Message, state: FSMContext):
 
 
 if __name__ == '__main__':
+    executor.start(dp, db_on_startup())
     executor.start_polling(dp, skip_updates=True)
